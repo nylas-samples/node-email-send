9cd474dd964f1e906d505fe4448eb0ca9f73e1b8
Ash <sayhi@ashryan.io>
2022-04-05 11:49:40 -0400

    Minor cleanup.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import Nylas from "nylas";
-import draftClass from "nylas/lib/models/draft.js";
-const { default: Draft } = draftClass;
+import Draft from "nylas/lib/models/draft.js";
 
 dotenv.config();
 
@@ -12,11 +11,15 @@ Nylas.config({
 
 const nylas = Nylas.with(process.env.ACCESS_TOKEN);
 
-const draft = new Draft(nylas, {
+const draft = new Draft.default(nylas, {
   subject: "With Love, from Nylas",
   body: "Well well well...",
-  to: [{ name: "Sender name", email: process.env.SENDER_ADDRESS }],
+  to: [{ name: "Recipient name", email: process.env.SENDER_ADDRESS }],
 });
 
-const message = await draft.send();
-console.log(`${message.id} was sent`);
+try {
+  const message = await draft.send();
+  console.log(`Message "${message.subject}" was sent with ID ${message.id}`);
+} catch (err) {
+  console.log(err);
+}
