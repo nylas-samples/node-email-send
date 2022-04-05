879051e03d3539662e9ee1632d7427ce94adb6fa
Ash <sayhi@ashryan.io>
2022-04-05 14:10:13 -0400

    Added comments and error handler.

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
+// Import your dependencies
 import dotenv from "dotenv/config.js";
 import Nylas from "nylas";
 import Draft from "nylas/lib/models/draft.js";
 
+// Configure your Nylas client
 Nylas.config({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 });
-
 const nylas = Nylas.with(process.env.ACCESS_TOKEN);
 
+// Create a draft email
 const draft = new Draft.default(nylas, {
   subject: "With Love, from Nylas",
   body: "Well well well...",
-  to: [{ name: "Recipient name", email: process.env.SENDER_ADDRESS }],
+  to: [{ name: "Recipient name", email: "process.env.SENDER_ADDRESS" }],
 });
 
+// Send the email
 try {
   const message = await draft.send();
   console.log(`Message "${message.subject}" was sent with ID ${message.id}`);
 } catch (err) {
-  console.log(err);
+  console.error("Error:\n", err);
 }
